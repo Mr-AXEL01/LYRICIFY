68a6c3d901a6d3adaae84dbe8a5fd19ad9bbbb98
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-18 12:27:46 +0100

    update the id client

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 const AUTH_URL =
     "https://accounts.spotify.com/authorize/" +
-    "?client_id=f342fb5df61e48d8b145b3bf91fc2560" +
+    "?client_id=4f2ed15a0d204edf91688cae04c31c29" +
     "&response_type=code" +
     "&redirect_uri=http://localhost:3000" +
     "&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
