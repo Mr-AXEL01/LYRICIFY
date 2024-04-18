a33faefe3c68647f7c83366b6b422cf0829c6d55
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-18 14:29:08 +0100

    trying lyrics-finder package for the displaying lyrics

--- src/TrackSearchResult.jsx
+++ src/TrackSearchResult.jsx
@@ -4,7 +4,7 @@ export default function TrackSearchResult({ track, chooseTrack }) {
     }
     return (
         <div className="flex items-center m-2" onClick={handlePlay}>
-            <img src={track.albumUrl} className="h-[64px] w-[64px] cursor-pointer " alt={track.title} />
+            <img src={track.albumUri} className="h-[64px] w-[64px] cursor-pointer " alt={track.title} />
             <div className="ml-3">
                 <div>{track.title}</div>
                 <div><small>{track.artist}</small></div>
